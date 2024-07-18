822e7a8ad93798c1f54b56b1be267ef5d31b13bc
User <abbosbeknormuratov2003@gmail.com>
2024-07-18 15:35:30 +0500

    tekshirib ko'rayapman

--- app/src/main/java/uz/androbeck/virtualbank/ui/MainActivity.kt
+++ app/src/main/java/uz/androbeck/virtualbank/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import uz.androbeck.virtualbank.R
 //Bug fixed
+//what's up
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
